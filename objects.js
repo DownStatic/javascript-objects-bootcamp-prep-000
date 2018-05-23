0abf248e727b197f1f89a39dc8ffa1a04a64b046
Christopher Stull <chris.stull@gmail.com>
2018-05-23 21:49:27 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = {}
+
+function updatePlaylist(playlist,artist,song){
+  playlist.artist = song
+  /* with this structure, the playlist cannot have more than 1 song
+  by the same artist */
+  return playlist
+}
+
+function removeFromPlaylist(playlist,artist){
+  delete playlist.artist
+  return playlist
+}
+
